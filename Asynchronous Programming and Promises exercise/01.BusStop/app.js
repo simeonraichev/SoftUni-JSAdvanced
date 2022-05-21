24c6ac2331a9e2dc21b0c836809851840a0a6b6b
Simeon Raychev <simeon0022@gmail.com>
2022-05-21 12:07:54 +0300

    Final code of the 01.Bus

--- Asynchronous Programming and Promises exercise/01.BusStop/app.js
+++ Asynchronous Programming and Promises exercise/01.BusStop/app.js
@@ -1,8 +1,29 @@
 async function getInfo(){
-    const  stopId = document.getElementById('stopId');
+    const stopId = document.getElementById('stopId');
     const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId.value}`;
+    const stopNameEl = document.getElementById('stopName');
+    const timetableEl = document.getElementById('buses');
 
-    const res  =  await fetch(url);
-    const data = await res.json();
-    console.log(data);
+
+    
+    try{
+        stopNameEl.textContent = "Loading...";
+        timetableEl.replaceChildren();
+        const res  =  await fetch(url);
+        if(res.status != 200){
+            // alert('Stop id not found!')
+            throw new Error("Stop id not found");
+        }
+        const data = await res.json();
+        stopNameEl.textContent = data.name;
+        console.log(data);
+        Object.entries(data.buses).forEach(b=>{
+            const liElement = document.createElement('li');
+            liElement.textContent = `Bus ${b[0]} arrives is ${b[1]} minutes`;
+            timetableEl.appendChild(liElement);
+        })
+    }catch(error){
+        stopNameEl.textContent = "Stop ID not found!"
+
+    }
 }
