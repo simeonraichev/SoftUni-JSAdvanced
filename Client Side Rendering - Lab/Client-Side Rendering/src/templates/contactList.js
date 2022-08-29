c39e1763bbbb97bc8f3da863bdce37c1cd53c8e9
Simeon Raychev <simeon0022@gmail.com>
2022-08-29 23:38:00 +0300

    Client-Side Rendering Templates edit

--- Client Side Rendering - Lab/Client-Side Rendering/src/templates/contactList.js
+++
@@ -1,9 +0,0 @@
-import contactTemplate from './contact.js';
-
-const contactListTemplate = (contacts) => `
-    <div class="contact-list" style="display: flex; justify-content: space-around;">
-        ${contacts.map(x => contactTemplate(x)).join('')}
-    </div>
-`;
-
-export default contactListTemplate;
